44239adb20c391d12c609ad90483c1e705e0e591
maxim199921 <maxim199921@rambler.ru>
2018-12-22 12:42:23 +0300

    hi

---
+++ homework-2/ishop/IshopTable.js
@@ -0,0 +1,64 @@
+﻿var IshopTable = React.createClass({
+
+    displayName: 'IshopTable',
+
+    propTypes: {
+        name: React.PropTypes.string.isRequired,
+        ishopList: React.PropTypes.arrayOf(
+            React.PropTypes.shape({
+                name: React.PropTypes.string.isRequired,
+                price: React.PropTypes.number.isRequired,
+                url: React.PropTypes.string.isRequired,
+                quantity: React.PropTypes.number.isRequired,
+            })
+        )
+    },
+
+    getInitialState: function () {
+        return {
+            ishopList: this.props.ishopList,
+        };
+    },
+
+    delete: function (code) {
+        if (confirm('Вы уверены?')) {
+            this.setState({
+                    ishopList: this.state.ishopList.filter((item) => {
+                            return item.code !== code;
+                        }
+                    )
+                }
+            );
+        }
+    },
+
+    changeBackgroundItem: function (evt) {
+        if (evt.currentTarget.style.background === 'red') {
+            evt.currentTarget.style.background = 'khaki';
+        } else {
+            evt.currentTarget.style.background = 'red';
+        }
+    },
+
+    render: function () {
+
+        var ishopListResult = this.state.ishopList.map((item) =>
+            React.DOM.div({key: item.code, className: 'tableItem', onClick: this.changeBackgroundItem},
+                React.DOM.span({className: 'nameItem'}, item.name),
+                React.DOM.span({className: 'price'}, item.price),
+                React.DOM.span({className: 'url'}, item.url),
+                React.DOM.span({className: 'quantity'}, item.quantity),
+                React.DOM.button({
+                    className: 'delete', onClick: () => {
+                        return this.delete(item.code);
+                    }
+                }, 'delete'),
+            )
+        );
+        return React.DOM.div({className: 'ishopTable'},
+            React.DOM.div({className: 'nameShop'}, this.props.name),
+            React.DOM.div({className: 'ishoplist',}, ishopListResult),
+        );
+    },
+
+});
