4dab380b774fccc780dc92fccaaff220e7dbe304
maxim199921 <maxim199921@rambler.ru>
2019-01-02 14:00:09 +0300

    hi

--- homework-2/filter/Filter.js
+++ homework-2/filter/Filter.js
@@ -5,10 +5,10 @@ let Filter = React.createClass({
     propTypes: {
         text: React.PropTypes.string.isRequired,
         checkstatus: React.PropTypes.bool.isRequired,
-        filterList:React.PropTypes.arrayOf(
+        filterList: React.PropTypes.arrayOf(
             React.PropTypes.shape({
                 string: React.PropTypes.string.isRequired,
-                code: React.PropTypes.number .isRequired,
+                code: React.PropTypes.number.isRequired,
             })
         )
     },
@@ -22,36 +22,41 @@ let Filter = React.createClass({
     },
 
     filterFunc: function (filterList) {
-        return filterList.filter( (item) => {
+        return filterList.filter((item) => {
             return item.string.indexOf(this.state.text) > -1
         });
     },
 
     filterValue: function (evt) {
-        this.setState( {text: evt.target.value}, this.arrResult );
+        this.setState({text: evt.target.value}, this.arrResult);
     },
 
-    workMode: function () {
-        this.state.checkstatus === false
-            ? this.setState( {checkstatus: true}, this.arrResult )
-            : this.setState( {checkstatus: false}, this.arrResult);
+    workMode: function (evt) {
+        this.setState({checkstatus: evt.target.checked}, this.arrResult);
     },
 
     arrResult: function () {
-        this.state.filterList =  this.state.checkstatus === true
-            ? this.setState( {filterList: this.filterFunc(this.props.filterList).sort((x, y) => (y.string > x.string) ? -1 : null)})
-            : this.setState( {filterList: this.filterFunc(this.props.filterList)});
+        let filterArr = this.filterFunc(this.props.filterList);
+        let finelArr = this.state.checkstatus
+            ? filterArr.sort((x, y) => (y.string > x.string) ? -1 : 0)
+            : filterArr;
+        this.setState( {filterList: finelArr} );
     },
 
-    render: function() {
+    render: function () {
 
-        let filterListResult = this.state.filterList.map( (item) =>
-            React.DOM.li({key:item.code, className:'filtritem'},item.string)
+        let filterListResult = this.state.filterList.map((item) =>
+            React.DOM.li({key: item.code, className: 'filtritem'}, item.string)
         );
-        return React.DOM.div( {className:'main-filter'},
-            React.DOM.input( {className:'sort', type: 'checkbox', onClick: this.workMode}),
-            React.DOM.input( {className:'input-filter', type: 'text', defaultValue: this.state.text, onChange: this.filterValue},),
-            React.DOM.ul( {className:'ishoplist'}, filterListResult ),
+        return React.DOM.div({className: 'main-filter'},
+            React.DOM.input({className: 'sort', type: 'checkbox', onClick: this.workMode}),
+            React.DOM.input({
+                className: 'input-filter',
+                type: 'text',
+                defaultValue: this.state.text,
+                onChange: this.filterValue
+            },),
+            React.DOM.ul({className: 'ishoplist'}, filterListResult),
         );
     },
 
