038c584d5ee7b9c6f1355c8b0322c1fccb27751a
maxim199921 <maxim199921@rambler.ru>
2019-01-09 22:20:30 +0300

    events

--- homework-4/ishop/components/events.js
+++ homework-4/ishop/components/events.js
@@ -1,9 +1,9 @@
 import {EventEmitter} from 'events';
 
 let ishopEvents=new EventEmitter();
-// в потоке voteEvents будут все события, связанные с голосованием
-// событие "EAnswerClicked" - кликнут вариант ответа, его сэмиттирует VotesAnswer и примет VotesBlock
-// событие "EFreeAnswerTextChanged" - изменён текст свободного ответа, его сэмиттирует VotesAnswer и примет VotesBlock
+// в потоке ishopEvents будут все события, связанные добавлением удалением элементов ishop
+// событие "evtSaveEdit" - кликнута кнопка сохранения изменения элемента списка ishop, его сэмиттирует IshopForms и примет IshopTable
+// событие "evtInitWorkMode" - кликнута кнопка закрыть вкладку редактирования элемента, его сэмиттирует IshopForms и примет IshopTable
 // лучше работать не с текстовыми литералами, а объявить переменные с соответствующими значениями
 
 export {ishopEvents};
