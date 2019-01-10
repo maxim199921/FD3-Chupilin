5415e34b4115344a801c5b5a245b490fcc2040e4
maxim199921 <maxim199921@rambler.ru>
2019-01-10 20:19:05 +0300

    hi

---
+++ homework-4/br2jsx/components/br2jsx.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import './br2jsx.css';
+
+class Br2jsx extends React.Component {
+
+    static propTypes = {
+        string: PropTypes.string.isRequired,
+    };
+
+    render() {
+        return (
+            <div>
+                {this.props.string.split(/<br ?\/?>/).map((el) => {
+                    return el = [el, <br key={el}/>];
+                })}
+            </div>
+        );
+    }
+
+}
+
+export default Br2jsx;
