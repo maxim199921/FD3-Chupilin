58cc13393da6a7e70b6320a1e4df68899c9cd07f
maxim199921 <maxim199921@rambler.ru>
2019-01-10 21:24:48 +0300

    hi

---
+++ homework-4/br2jsx/App.js
@@ -0,0 +1,14 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import Br2jsx from './components/br2jsx';
+
+let string="первый<br>второй<br/>третий<br />последний<br />предпоследний";
+
+ReactDOM.render(
+    <Br2jsx string={string}/>, document.getElementById('container')
+);
+
+
--- homework-4/br2jsx/bundle.js
+++ homework-4/br2jsx/bundle.js
@@ -24081,8 +24081,6 @@ Object.defineProperty(exports, "__esModule", {
     value: true
 });
 
-var _createClass = function () { function defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } } return function (Constructor, protoProps, staticProps) { if (protoProps) defineProperties(Constructor.prototype, protoProps); if (staticProps) defineProperties(Constructor, staticProps); return Constructor; }; }();
-
 var _react = __webpack_require__(2);
 
 var _react2 = _interopRequireDefault(_react);
@@ -24095,40 +24093,20 @@ __webpack_require__(22);
 
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
-
-function _possibleConstructorReturn(self, call) { if (!self) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return call && (typeof call === "object" || typeof call === "function") ? call : self; }
-
-function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function, not " + typeof superClass); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, enumerable: false, writable: true, configurable: true } }); if (superClass) Object.setPrototypeOf ? Object.setPrototypeOf(subClass, superClass) : subClass.__proto__ = superClass; }
-
-var Br2jsx = function (_React$Component) {
-    _inherits(Br2jsx, _React$Component);
-
-    function Br2jsx() {
-        _classCallCheck(this, Br2jsx);
-
-        return _possibleConstructorReturn(this, (Br2jsx.__proto__ || Object.getPrototypeOf(Br2jsx)).apply(this, arguments));
-    }
-
-    _createClass(Br2jsx, [{
-        key: 'render',
-        value: function render() {
-            return _react2.default.createElement(
-                'div',
-                null,
-                this.props.string.split(/<br ?\/?>/).map(function (el) {
-                    return el = [el, _react2.default.createElement('br', { key: el })];
-                })
-            );
-        }
-    }]);
-
-    return Br2jsx;
-}(_react2.default.Component);
+var Br2jsx = function Br2jsx(props) {
+    return _react2.default.createElement(
+        'div',
+        null,
+        props.string.split(/<br ?\/?>/).map(function (el) {
+            return el = [el, _react2.default.createElement('br', { key: el })];
+        })
+    );
+};
 
 Br2jsx.propTypes = {
     string: _propTypes2.default.string.isRequired
 };
+
 exports.default = Br2jsx;
 
 /***/ }),
