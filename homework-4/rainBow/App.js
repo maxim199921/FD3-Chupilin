65e231117b945d7a86239163db8f1dd2a5f57986
maxim199921 <maxim199921@rambler.ru>
2019-01-10 15:48:52 +0300

    hi

---
+++ homework-4/rainBow/App.js
@@ -0,0 +1,16 @@
+﻿"use strict";
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import RainbowFrame from './components/RainbowFrame';
+
+let color = ['red', 'orange', 'yellow', 'green', '#00BFFF', 'blue', 'purple'];
+
+
+ReactDOM.render(
+    <RainbowFrame color={color}>{'RAINBOW RAINBOW RAINBOW RAINBOW RAINBOW RAINBOW RAINBOW RAINBOW RAINBOW'}</RainbowFrame>
+    , document.getElementById('container')
+);
+
+
---
+++ homework-4/rainBow/components/RainbowFrame.js
@@ -0,0 +1,26 @@
+﻿import React from 'react';
+import PropTypes from 'prop-types';
+
+import './RainbowFrame.css';
+
+class RainbowFrame extends React.Component {
+
+    static propTypes = {
+        color: PropTypes.array.isRequired,
+    };
+
+    render() {
+        return (
+            this.props.color.length !== 0
+            ? <div style={{border: "solid 34px " + this.props.color[0], padding: "10px"}}>
+                    <RainbowFrame color={this.props.color.splice(1)}>
+                        {this.props.children}
+                    </RainbowFrame>
+               </div>
+            : <span>{this.props.children}</span>
+        );
+    }
+
+}
+
+export default RainbowFrame;
